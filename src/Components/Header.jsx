70e0ea44533424ef9291325f7c4d5c87772391b9
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-04 12:56:11 +0100

    4:55 4/8

---
+++ src/Components/Header.jsx
@@ -0,0 +1,13 @@
+import Chef from '../Imgs/chef.png'
+
+export function Header()
+{
+    return (
+        <>
+            <header>
+                <img src={Chef} alt="" />
+                <h3>Chef Claude</h3>
+            </header>
+        </>
+    )
+}
---
+++ src/Components/Main.jsx
@@ -0,0 +1,11 @@
+export function Main()
+{
+    return (
+        <main className="MainContent">
+            <form action="">
+                <input type="text" className="InputField" placeholder="Add the ingrediant ..."/>
+                <button type="submit" className="SubmitButton">+ Add</button>
+            </form>
+        </main>
+    )
+}
