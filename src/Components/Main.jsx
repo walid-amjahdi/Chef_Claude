f116991f2570c237ce758ba9e321a3c08d54db0e
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-05 12:36:22 +0100

    5:10 5/8

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
+
 export function Main()
 {
+    const [ingrediants,setIngrediants] = useState(["ing1","ing2","ing3"])
+
+    const handleSubmit = (event) =>
+    {
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const ingrediant = formData.get("ingrediant")
+        setIngrediants(...ingrediants,ingrediant)
+    }
+
     return (
-        <main className="MainContent">
-            <form action="">
-                <input type="text" className="InputField" placeholder="Add the ingrediant ..."/>
-                <button type="submit" className="SubmitButton">+ Add</button>
-            </form>
-        </main>
+        <>
+            <main className="MainContent" >
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className="InputField" placeholder="Add the ingrediant ..." name="ingrediant"/>
+                    <button type="submit" className="SubmitButton">+ Add</button>
+                </form>
+            </main>
+            <ul className="IngrediantsList">
+                {ingrediants.map((ing,index)=>(
+                    <li key={index}>{ing}</li>
+                ))}
+            </ul>
+        </>
     )
 }
