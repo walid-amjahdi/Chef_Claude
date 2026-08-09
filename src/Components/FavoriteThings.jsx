8559775d46e663ecc4c9760ad3ec33e3547ac0f8
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-09 11:02:50 +0100

    6:02 9/8

--- src/Components/FavoriteThings.jsx
+++ src/Components/FavoriteThings.jsx
@@ -4,8 +4,18 @@ export function FavoriteThings()
 {
     const [myFavorite,setMyFavorite] = useState([])
     const [favoriteThings,setFavoriteThings] = useState(["1","2","3","4"])
+
+    function addThings()
+    {
+        setMyFavorite(prevThings => [...prevThings,"test"])
+    }
+
     return (
         <>
+            <button className="SubmitButton" 
+            onClick={addThings}>
+                +Add
+            </button>
             <ul className="IngrediantsList">
                 {myFavorite.map((thing,index)=>(
                     <li key={index}>{thing}</li>
