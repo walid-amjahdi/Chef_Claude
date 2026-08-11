fcc65cee6a6867c804b2fb8474829ff56438aa88
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-11 10:08:30 +0100

    6:39 11/8

--- src/Components/FavoriteThings.jsx
+++ src/Components/FavoriteThings.jsx
@@ -2,14 +2,19 @@ import { useState } from "react"
 
 export function FavoriteThings()
 {
+    function logIn(formData)
+    {
+        console.log(formData.get("userName"))
+    }
     return (
         <>
-            <form action="">
+            <form action={logIn}>
                 <label htmlFor="userName">UserName : </label>
                 <input type="text" name="userName" id="userName" />
                 <br />
                 <label htmlFor="password">Password : </label>
                 <input type="password" name="password" id="password" />
+                <button>log in</button>
             </form>
         </>
     )
