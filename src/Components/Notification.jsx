cda9b3143374f676ea58d86353135869f81ff734
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-13 11:42:26 +0100

    7:35 13/8

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -5,7 +5,8 @@ export function Notification()
     const [notifications,setNotifications] = useState(["a","b"])
     return (
         <>
-            {(notifications.length !== 0) && <h3>you have notifications !!!</h3>}        
+            {(notifications.length > 0) && <h3>you have notifications !!!</h3>}        
+            {(notifications.length === 0) && <h3>you have no notifications !!!</h3>}        
         </>
     )
 }
