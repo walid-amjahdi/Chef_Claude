8af691955d0dac1e866f9aeca98c3edcadb0f529
walid-amjahdi <w.amjahdi3996@uca.ac.ma>
2026-08-13 12:00:57 +0100

    7:40 13/8

--- src/Components/Notification.jsx
+++ src/Components/Notification.jsx
@@ -2,10 +2,20 @@ import { useState } from "react"
 
 export function Notification()
 {
-    const [notifications,setNotifications] = useState(["a","b"])
+    const [notifications,setNotifications] = useState([])
+
+    function showNotifications()
+    {
+        if(notifications.length === 0)
+            return <h3>you have no notifications !!!</h3>
+        else if(notifications.length === 1)
+            return <h3>you have 1 notification !!!</h3>
+        else 
+            return <h3>you have {notifications.length} notifications !!!</h3>
+    }
     return (
         <>
-            {(notifications.length > 0) ? <h3>you have notifications !!!</h3> : <h3>you have no notifications !!!</h3>}        
+            {showNotifications()}        
         </>
     )
 }
